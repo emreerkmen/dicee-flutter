e05e79f38d9f60eeb690e76d4990097684355ae4
emreerkmen <emre.erkmen@hotmail.com>
2020-04-19 17:28:21 +0300

    last app for dicee app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -6,7 +7,9 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
-          title: Text('Dicee'),
+          title: Center(
+            child: Text('Dicee'),
+          ),
           backgroundColor: Colors.red,
         ),
         body: DicePage(),
@@ -15,27 +18,61 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+//stful is short-key for statefull widget.
+
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+  void changeLeftDiceNumber(){
+    setState(() {
+      leftDiceNumber= Random().nextInt(6)+1;
+      print('Left button get pressed.Number = $leftDiceNumber');
+    });
+  }
+
+  //When ever press the hotreload this method rerun again. So if you want to incled the changes about your variable
+  //you have to put the varaible in the this method.
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Row(
-        children: <Widget>[
-          Expanded(
-            //flex: 2,
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Image.asset('images/dice2.png'),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Expanded(
+                  //flex: 2,
+                  child: FlatButton(
+                    child: Image.asset(
+                        'images/dice$leftDiceNumber.png'), //this dolar thing provides 'String Interpolation'
+                    onPressed: () {
+                      changeLeftDiceNumber();
+                    },
+                  ),
+                ),
+                Expanded(
+                  //flex: 1,
+                  child: FlatButton(
+                    child: Image.asset('images/dice$rightDiceNumber.png'),
+                    onPressed: () {
+                      setState(() {
+                        rightDiceNumber= Random().nextInt(6)+1;
+                        print('Right button get pressed.Number = $rightDiceNumber');
+                      });
+                    },
+                  ),
+                ),
+              ],
             ),
-          ),
-          Expanded(
-            //flex: 1,
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Image.asset('images/dice1.png'),
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
